0af60e9dc6f902fa970414ef6b0a74a1cd5cf5be
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2024-07-21 04:06:40 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -269,7 +269,7 @@ def predict_numbers_and_accuracy(models):
     return model_predictions
 
 # Button to predict winning lotto numbers and display analysis
-if st.button('Predict 5 sets Winning Lotto Numbers by 5 ML Models'):
+if st.button('Predict 5 sets Next Winning Lotto Numbers by 5 ML Models'):
     predictions = predict_numbers_and_accuracy(models)
     predictions_df = pd.DataFrame(predictions).T.reset_index()
     predictions_df.columns = ['Model', 'Predicted Numbers', 'Predicted Accuracy (%)']
